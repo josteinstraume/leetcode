4e60b5b135ca05de3c8534c72c66fb6553e94d8e
Jostein Straume <joebarrystraume@gmail.com>
2017-12-13 18:39:55 -0800

    two sum exercise complete

--- top_interview_questions/1_two_sum.py
+++ top_interview_questions/1_two_sum.py
@@ -1,7 +1,7 @@
 # Given an array of integers,
 # return indices of the two numbers such that they add up to a specific target.
 
-# You may assume that each input would have exactly one solution, 
+# You may assume that each input would have exactly one solution,
 # and you may not use the same element twice.
 
 # Example:
@@ -9,3 +9,44 @@
 
 # Because nums[0] + nums[1] = 2 + 7 = 9,
 # return [0, 1].
+
+# Initial solution
+# Not accepted since returns a string
+# O(n-squared) run time
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+
+    n = len(nums)
+    for i in range(1, n):
+        for j in range(2, n):
+            if nums[i] + nums[j] == target:
+                result = '[%d,%d]'%(i,j)
+                print(result)
+                return result
+
+
+nums = [2, 7, 11, 15]
+target = 9
+
+# O(n) run time
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        map = {}
+        n = len(nums)
+        for i in range(n):
+        	# target is 9, first num in nums is 2, so we're looking for 9-2=7 in nums
+            if nums[i] not in map:
+                map[target - nums[i]] = i
+            else:
+                return map[nums[i]], i
+        # does not add up to the given target
+        return -1, -1
